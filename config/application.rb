7cbcc333827da9da872379c5b4803fd0334a9b6c
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 12:48:23 +0000

    Enable sprockets railtie for Rails 7

--- config/application.rb
+++ config/application.rb
@@ -1,29 +1,16 @@
 require_relative "boot"
-
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Rottenpotatoes
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-  
+
+    # Enable Sprockets for Rails 7
+    require "sprockets/railtie"
+
     config.assets.initialize_on_precompile = false
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
