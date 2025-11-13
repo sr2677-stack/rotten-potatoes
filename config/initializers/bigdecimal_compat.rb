8933805574cd76d3eac311f2640aba9288187396
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 12:02:18 +0000

    Remove Rails 7 initializers causing ActionDispatch::Static crash

--- config/initializers/bigdecimal_compat.rb
+++
@@ -1,9 +0,0 @@
-# config/initializers/bigdecimal_compat.rb
-# Patch for Rails 4.2 compatibility with Ruby 2.7+
-if RUBY_VERSION >= '2.7' && !BigDecimal.respond_to?(:new)
-  class BigDecimal
-    def self.new(*args)
-      BigDecimal(*args)
-    end
-  end
-end
