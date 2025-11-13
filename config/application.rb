65575cbd023e1e08be7839c731841822c129dda4
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 12:54:15 +0000

    Fix ActionCable railtie

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative "boot"
+
 require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
@@ -6,20 +7,16 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
 require "action_mailer/railtie"
+require "action_cable/engine"
 require "action_mailbox/engine"
 require "action_text/engine"
-require "rails/test_unit/railtie"
 require "sprockets/railtie"
-
 
 Bundler.require(*Rails.groups)
 
 module Rottenpotatoes
   class Application < Rails::Application
     config.load_defaults 6.1
-
-    # Enable Sprockets for Rails 7
-    require "sprockets/railtie"
 
     config.assets.initialize_on_precompile = false
     config.autoload_lib(ignore: %w(assets tasks))
