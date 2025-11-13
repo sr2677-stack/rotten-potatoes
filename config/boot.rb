ef4f6ef68d39932fed489e59ace9a840b8ee3d33
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 12:07:36 +0000

    Fix boot.rb — remove Rails 7 bigdecimal patch

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
-
-require File.expand_path('../../bigdecimal_patch', __FILE__)
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
-
+require 'bundler/setup' # Set up gems listed in the Gemfile.
