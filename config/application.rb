d1b98817674af0db66012d472ff29e97b32e7890
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 08:35:40 +0000

    Fix ActionDispatch::Static patch placement

--- config/application.rb
+++ config/application.rb
@@ -1,29 +1,31 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+# PATCH FOR RUBY 3.1+ COMPATIBILITY - Add this BEFORE the module declaration
+if defined?(ActionDispatch::Static)
+  module ActionDispatch
+    class Static
+      def initialize(app, path, index: 'index', headers: {})
+        @app = app
+        @file_handler = FileHandler.new(path, index: index, headers: headers)
+      end
+    end
+  end
+end
 
 module Rottenpotatoes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # ADD THIS BLOCK HERE - Patch for Ruby 3.1+ compatibility
-    config.before_initialize do
-      require 'action_dispatch/middleware/static'
-      
-      ActionDispatch::Static.class_eval do
-        def initialize(app, path, index: 'index', headers: {})
-          @app = app
-          @file_handler = ActionDispatch::FileHandler.new(path, index: index, headers: headers)
-        end
-      end
-    end
-    # END OF PATCH
-
-    # rest of your configuration...
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
