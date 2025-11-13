08188a581f3d27107b8469c56fcc3c5a6bb04f43
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 08:40:24 +0000

    Apply ActionDispatch::Static patch in boot.rb for Ruby 3.1

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,18 @@
 require File.expand_path('../../bigdecimal_patch', __FILE__)
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
+# Patch for Ruby 3.1+ and Rails 5.2 compatibility
+# This must be here in boot.rb to load before the middleware stack
+require 'action_dispatch/middleware/static'
+
+module ActionDispatch
+  class Static
+    def initialize(app, path, index: 'index', headers: {})
+      @app = app
+      @file_handler = FileHandler.new(path, index: index, headers: headers)
+    end
+  end
+end
--- config/initializers/action_dispatch_static_patch.rb
+++
@@ -1,9 +0,0 @@
-# Patch ActionDispatch::Static for Ruby 3.1+ compatibility
-Rails.application.config.after_initialize do
-  ActionDispatch::Static.class_eval do
-    def initialize(app, path, index: 'index', headers: {})
-      @app = app
-      @file_handler = ActionDispatch::FileHandler.new(path, index: index, headers: headers)
-    end
-  end
-end
