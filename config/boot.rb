f0cfb96721ac9cc56336e9d80cd6f6bdf1b0b07a
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 08:41:43 +0000

    Handle missing bootsnap gem gracefully

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,13 @@ require File.expand_path('../../bigdecimal_patch', __FILE__)
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# Only require bootsnap if it's available
+begin
+  require 'bootsnap/setup'
+rescue LoadError
+  # Bootsnap not available, skip it
+end
 
 # Patch for Ruby 3.1+ and Rails 5.2 compatibility
 # This must be here in boot.rb to load before the middleware stack
