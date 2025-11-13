1f2c7139131423b27e387f51303bf10e6cdfe98e
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 09:32:13 +0000

    changes

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source 'https://rubygems.org'
-ruby '3.1.4'  # Change this from 3.1.4
+ruby '3.1.2'
 
 gem 'rails', '5.2.8.1'
-gem 'rack', '2.2.8'
 gem 'puma', '~> 4.3'
 
 gem 'sass-rails', '~> 5.0'
--- config/boot.rb
+++ config/boot.rb
@@ -2,24 +2,5 @@
 require File.expand_path('../../bigdecimal_patch', __FILE__)
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require "bundler/setup" # Set up gems listed in the Gemfile.
 
-# Only require bootsnap if it's available
-begin
-  require 'bootsnap/setup'
-rescue LoadError
-  # Bootsnap not available, skip it
-end
-
-# Patch for Ruby 3.1+ and Rails 5.2 compatibility
-# Wait until ActionDispatch is actually loaded
-Rails.application.config.before_initialize do
-  ActionDispatch::Static.class_eval do
-    remove_method :initialize if method_defined?(:initialize)
-    
-    def initialize(app, path, index: 'index', headers: {})
-      @app = app
-      @file_handler = ActionDispatch::FileHandler.new(path, index: index, headers: headers)
-    end
-  end
-end
