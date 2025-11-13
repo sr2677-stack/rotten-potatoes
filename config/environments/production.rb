3db9e8c26fbe986c7a46eb24fb431abd1ba8476e
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 10:19:22 +0000

    Fix Rails 6.1 logger config for Ruby 3.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,31 +2,16 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
 
-  config.consider_all_requests_local = false
-  config.action_controller.perform_caching = true
-
-  # FIX FOR HEROKU + RAILS 5.2
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # JS compressor
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  
   config.assets.js_compressor = Uglifier.new(harmony: true)
-
-  # Asset pipeline
   config.assets.compile = true
   config.assets.digest = true
 
   config.log_level = :info
-  config.log_tags = [:request_id]
-
-  config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.new
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  
+  config.logger = ActiveSupport::Logger.new($stdout)
+  config.logger.formatter = Logger::Formatter.new
 
   config.active_record.dump_schema_after_migration = false
 end
