a9b93e34ce7354d37cfcda162a17779db828588d
Shivani2211-web <rokkamshivanireddy322@gmail.com>
2025-11-13 13:12:58 +0000

    Fix migration version

--- db/migrate/20251104062330_create_movies.rb
+++ db/migrate/20251104062330_create_movies.rb
@@ -1,5 +1,5 @@
-class CreateMovies < ActiveRecord::Migration
-  def change
+class CreateMovies < ActiveRecord::Migration[6.1]  
+def change
     create_table :movies do |t|
       t.string :title
       t.string :rating
